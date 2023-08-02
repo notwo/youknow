929356be6134a3856b28f6f4a006a24f9201c335
T.M <ace071144@gmail.com>
2023-08-02 21:24:47 +0900

    キーワード追加

--- you_know/urls.py
+++ you_know/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import top, user_auth, user_setting, library, category
+from .views import top, user_auth, user_setting, library, category, keyword
 
 app_name = 'you_know'
 urlpatterns = [
@@ -20,4 +20,5 @@ urlpatterns = [
   path('user_setting/user/delete_account_reason_done/', user_setting.UserDeleteAccountReasonDoneView.as_view(), name='delete_account_reason_done'),
   path('library/', library.LibraryIndexView.as_view(), name='library_index'),
   path('category/', category.CategoryIndexView.as_view(), name='category_index'),
+  path('keyword/', keyword.KeywordIndexView.as_view(), name='keyword_index'),
 ]
